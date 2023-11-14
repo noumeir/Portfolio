4d26c0ccc0c009df18b294bb284d601a25788499
Noumeir Morales <noumeir.morales@gmail.com>
2023-11-14 13:35:46 -0500

    index.php
    welcome page for Travel Genius

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE>
+<html>
+<head>
+	<center><title> Welcome to Travel Genius </title>
+	<style rel="stylesheet" type="text/css">
+
+	html {
+	background-color: pink;
+	font-family: "DejaVu Sans", "Liberation Sans", sans-serif;
+	font-size: 0.99em;
+	line-height: 1.25em;
+	margin: 0 4% 0 4%;
+	}
+
+	body {
+	border: 10px solid #fff;
+	margin:0;
+	padding:0;
+	background: pink;
+	}
+
+	a:link, a:visited (
+		color: #38488f;
+		text-decoration: none:
+	0media (max-width: 700px) (
+		body(
+			background-color: #fff;
+		)
+		div (
+			width:auto;
+			margin: 0 auto;
+			border-radius: 0;
+			padding: lem;
+		    )
+	)
+</style>
+</head>
+
+</body>
+</div>
+	</h1> Travel Genius </h1>
+	
+	<br>Welcome!
+	<p>This domain is made for people who want to know the best places to travel in  that season. No one wants to make the trip out to Hawaii only to find out
+	   it is hurricane season. Let us guide you to your next best vacation!</p>
+ 
+	<center><img src="bungalow.jpeg" onmouseover="imgLarge(this)" onmouseout="imgRegular(this)" height="200" width="550">
+
+
+	
+	<hr>
+	<p style="font-size: 20px; color:#1F9AFE; text-align: center">
+	<a href="login.html">Login</a>
+	<br><a href="java.html">Create Account</a>
+        <br><a href="error.html">Exit</a>
+</div>
+</body>
+</html>
